c322484b14baab2bf85cc2b7f02a12fbb8a1db3c
Everton Rodrigo Tefili <tefili1978@yahoo.com.br>
2021-10-29 16:37:35 -0300

    DepartmentDao and SellerDao interfaces created

---
+++ src/main/java/com/jdbc1/udemy_jdbc/model/dao/DepartmentDao.java
@@ -0,0 +1,14 @@
+package com.jdbc1.udemy_jdbc.model.dao;
+
+import com.jdbc1.udemy_jdbc.model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+}
---
+++ src/main/java/com/jdbc1/udemy_jdbc/model/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package com.jdbc1.udemy_jdbc.model.dao;
+
+import com.jdbc1.udemy_jdbc.model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
