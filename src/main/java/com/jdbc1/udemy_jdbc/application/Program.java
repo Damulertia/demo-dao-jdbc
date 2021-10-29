3aad0b7894942ed656ddc03c3b9d6f537d470823
Everton Rodrigo Tefili <tefili1978@yahoo.com.br>
2021-10-29 18:27:17 -0300

    Program and SellerDaoJDBC improved reusing instantiation

--- src/main/java/com/jdbc1/udemy_jdbc/application/Program.java
+++ src/main/java/com/jdbc1/udemy_jdbc/application/Program.java
@@ -2,7 +2,6 @@ package com.jdbc1.udemy_jdbc.application;
 
 import com.jdbc1.udemy_jdbc.model.dao.DaoFactory;
 import com.jdbc1.udemy_jdbc.model.dao.SellerDao;
-import com.jdbc1.udemy_jdbc.model.entities.Department;
 import com.jdbc1.udemy_jdbc.model.entities.Seller;
 
 import java.util.Date;
@@ -11,14 +10,10 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Computers");
-        System.out.println(obj);
-
-        Seller seller = new Seller(1, "TON", "ton@gmail", new Date(), 6700.0, obj);
-        System.out.println(seller);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        Seller seller1 = sellerDao.findById(7);
-        System.out.println(seller1);
+
+        System.out.println("==== TEST 01: seller findById ====");
+        Seller seller = sellerDao.findById(7);
+        System.out.println(seller);
     }
 }
