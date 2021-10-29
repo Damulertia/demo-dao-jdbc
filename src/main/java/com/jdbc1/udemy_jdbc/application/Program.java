d1e45b3c9af5d286f95ab10933b607eebc8eb475
Everton Rodrigo Tefili <tefili1978@yahoo.com.br>
2021-10-29 16:01:26 -0300

    Department and Seller classes

--- src/main/java/com/jdbc1/udemy_jdbc/application/Program.java
+++ src/main/java/com/jdbc1/udemy_jdbc/application/Program.java
@@ -1,8 +1,12 @@
 package com.jdbc1.udemy_jdbc.application;
 
+import com.jdbc1.udemy_jdbc.model.entities.Department;
+
 public class Program {
 
     public static void main(String[] args) {
 
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
 }
