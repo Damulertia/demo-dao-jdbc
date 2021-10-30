b80c808057cc08be6b1a9e284a82fe33fa65c1ea
Everton Rodrigo Tefili <tefili1978@yahoo.com.br>
2021-10-29 21:15:17 -0300

    Find by Department implementation and test in Program

--- src/main/java/com/jdbc1/udemy_jdbc/application/Program.java
+++ src/main/java/com/jdbc1/udemy_jdbc/application/Program.java
@@ -5,6 +5,7 @@ import com.jdbc1.udemy_jdbc.model.dao.SellerDao;
 import com.jdbc1.udemy_jdbc.model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
 
@@ -15,5 +16,9 @@ public class Program {
         System.out.println("==== TEST 01: seller findById ====");
         Seller seller = sellerDao.findById(7);
         System.out.println(seller);
+
+        System.out.println("\n==== TEST 02: seller findByDepartment ====");
+        List<Seller> list = sellerDao.findByDepartment(seller.getDepartment());
+        list.forEach(System.out::println);
     }
 }
--- src/main/java/com/jdbc1/udemy_jdbc/model/dao/SellerDao.java
+++ src/main/java/com/jdbc1/udemy_jdbc/model/dao/SellerDao.java
@@ -1,5 +1,6 @@
 package com.jdbc1.udemy_jdbc.model.dao;
 
+import com.jdbc1.udemy_jdbc.model.entities.Department;
 import com.jdbc1.udemy_jdbc.model.entities.Seller;
 
 import java.util.List;
@@ -11,4 +12,5 @@ public interface SellerDao {
     void deleteById(Integer id);
     Seller findById(Integer id);
     List<Seller> findAll();
+    List<Seller> findByDepartment(Department department);
 }
